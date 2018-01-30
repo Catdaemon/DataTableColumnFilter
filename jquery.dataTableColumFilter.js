c166597d7cf6469b2d505b518cf7805331839fed
Carl Downie <Catdaemon@users.noreply.github.com>
2018-01-30 10:21:39 +0000

    Support multiple tables with the same selector

--- jquery.dataTableColumFilter.js
+++ jquery.dataTableColumFilter.js
@@ -18,113 +18,115 @@
     }
 
     $.fn.dataTableAdvFilter = function(options) {
-        // Add a class to the table for later reference
-        this.addClass("dtFilter");
-        var dt = $(this).DataTable();
+        this.each(function() {
+            // Add a class to the table for later reference
+            $(this).addClass("dtFilter");
+            var dt = $(this).DataTable();
 
-        // Get columns and add functionality to each
-        var columns = dt.columns();
+            // Get columns and add functionality to each
+            var columns = dt.columns();
 
-        for (let i = 0; i < columns.count(); i++)
-        {
-            let column = dt.column(i);
-            let header = column.header();
-            
-            // Filter column data to unique values
-            let unique = column.data().filter(function(value, index, self) {
-                return self.indexOf(value) === index;
-            });
-
-            // Sort items
-            unique = unique.sort(function (a, b) {
-                return a.localeCompare(b);
-            });
-
-            // Create filter elements
-            let filterContainer = $("<div class='dtFilters'>");
-            let filterButton = $("<a class='dtFilter'></a>");
-            let valueFilters = $("<ul></ul>");
-
-            // Text search box
-            let searchBox = $("<input type='text'></input>");
-            valueFilters.append("<li class='heading'>Search Values</li>");
-            valueFilters.append($("<li></li>").append(searchBox));            
-
-            // Checkboxes
-            valueFilters.append("<li class='heading'>Select Values</li>");
-            let selectAll = $("<input type='checkbox' checked> ");
-            valueFilters.append($("<label></label>").append(selectAll).append("Select All"));
-
-            // One checkbox for each unique value
-            for (let x = 0; x < unique.count(); x++)
+            for (let i = 0; i < columns.count(); i++)
             {
-                let item = unique[x];
-                let filter = $("<li></li>");
-                let label = $("<label></label>");
-                let input = $("<input type='checkbox' checked> ");
-                input.data("value", item);
-                label.append(input);
-                filter.append(label);
-                label.append(item);
-                valueFilters.append(filter);
-
-                // Search on change
-                input.on('change', function() {
-                    _columnSearchMulti(column, valueFilters);
+                let column = dt.column(i);
+                let header = column.header();
+                
+                // Filter column data to unique values
+                let unique = column.data().filter(function(value, index, self) {
+                    return self.indexOf(value) === index;
                 });
-            }
 
-            // Interaction
-            // Handle clicks
-            filterContainer.hide();
-            filterButton.on('click', function(e) {
-                filterContainer.css("top", filterButton.offset().top + filterButton.height());
-                filterContainer.css("left", filterButton.offset().left);
-                filterContainer.show();
-                e.stopPropagation();
-            });
-            $(document).on('click', function(e) {
-                if (filterContainer.has(e.target).length === 0) {
-                    filterContainer.hide();
+                // Sort items
+                unique = unique.sort(function (a, b) {
+                    return a.localeCompare(b);
+                });
+
+                // Create filter elements
+                let filterContainer = $("<div class='dtFilters'>");
+                let filterButton = $("<a class='dtFilter'></a>");
+                let valueFilters = $("<ul></ul>");
+
+                // Text search box
+                let searchBox = $("<input type='text'></input>");
+                valueFilters.append("<li class='heading'>Search Values</li>");
+                valueFilters.append($("<li></li>").append(searchBox));            
+
+                // Checkboxes
+                valueFilters.append("<li class='heading'>Select Values</li>");
+                let selectAll = $("<input type='checkbox' checked> ");
+                valueFilters.append($("<label></label>").append(selectAll).append("Select All"));
+
+                // One checkbox for each unique value
+                for (let x = 0; x < unique.count(); x++)
+                {
+                    let item = unique[x];
+                    let filter = $("<li></li>");
+                    let label = $("<label></label>");
+                    let input = $("<input type='checkbox' checked> ");
+                    input.data("value", item);
+                    label.append(input);
+                    filter.append(label);
+                    label.append(item);
+                    valueFilters.append(filter);
+
+                    // Search on change
+                    input.on('change', function() {
+                        _columnSearchMulti(column, valueFilters);
+                    });
                 }
-            });
 
-            // Text search box
-            searchBox.on('keyup change', function() {
-                var boxval = $(this).val();
-
-                // Hide checkboxes not matching the entered string
-                valueFilters.find("label").each(function() {
-                    if ($(this).text() == "Select All")
-                        return;
-
-                    if ($(this).text().toLowerCase().indexOf(boxval.toLowerCase()) !== -1 || boxval == "")
-                    {
-                        $(this).show();
-                    } else {
-                        $(this).hide();
+                // Interaction
+                // Handle clicks
+                filterContainer.hide();
+                filterButton.on('click', function(e) {
+                    filterContainer.css("top", filterButton.offset().top + filterButton.height());
+                    filterContainer.css("left", filterButton.offset().left);
+                    filterContainer.show();
+                    e.stopPropagation();
+                });
+                $(document).on('click', function(e) {
+                    if (filterContainer.has(e.target).length === 0) {
+                        filterContainer.hide();
                     }
                 });
 
-                // Check boxes based on their visibility
-                valueFilters.find("input[type='checkbox']").each(function() {
-                    $(this).prop('checked', $(this).is(":visible"));
+                // Text search box
+                searchBox.on('keyup change', function() {
+                    var boxval = $(this).val();
+
+                    // Hide checkboxes not matching the entered string
+                    valueFilters.find("label").each(function() {
+                        if ($(this).text() == "Select All")
+                            return;
+
+                        if ($(this).text().toLowerCase().indexOf(boxval.toLowerCase()) !== -1 || boxval == "")
+                        {
+                            $(this).show();
+                        } else {
+                            $(this).hide();
+                        }
+                    });
+
+                    // Check boxes based on their visibility
+                    valueFilters.find("input[type='checkbox']").each(function() {
+                        $(this).prop('checked', $(this).is(":visible"));
+                    });
+
+                    // Do table search
+                    _columnSearchMulti(column, valueFilters);
                 });
 
-                // Do table search
-                _columnSearchMulti(column, valueFilters);
-            });
+                // Select all
+                selectAll.on('change', function() {
+                    valueFilters.find("input[type='checkbox']:visible").prop('checked', $(this).is(":checked"));
+                    _columnSearchMulti(column, valueFilters);
+                });
 
-            // Select all
-            selectAll.on('change', function() {
-                valueFilters.find("input[type='checkbox']:visible").prop('checked', $(this).is(":checked"));
-                _columnSearchMulti(column, valueFilters);
-            });
-
-            // Add items to page
-            filterContainer.append(valueFilters);
-            $(header).append(filterButton);
-            $("body").append(filterContainer);
-        }
+                // Add items to page
+                filterContainer.append(valueFilters);
+                $(header).append(filterButton);
+                $("body").append(filterContainer);
+            }
+        });
     };
 })(jQuery);
